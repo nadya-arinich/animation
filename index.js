e29eb74c65eaa49dc5ca28ac19bd1096c7d6c063
nadya-arinich <97697219+nadya-arinich@users.noreply.github.com>
2022-09-29 21:48:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,26 +6,15 @@ $(function(){
 var planeta_id = 'canvas';
 
 var image = new Image();
-image.src = './4128.jpg';
+image.src = './planeta.jpg';
 
 // загружаем изображение тени и бликов планеты
-var fxShadow = new Image();
-fxShadow.src = './123.png';
+var img = new Image();
+img.src = './blik.png';
 
 // определяем длину и высоту элемента canvas
 var width = $('#'+planeta_id).width();
 var height = $('#'+planeta_id).height();
-
-// рассчитываем угол вращения планеты
-var beta = 360/1000;
-var beta = (beta*Math.PI)/180;
-var l = (Math.sqrt(width*width+width*width))/2;
-var gam = Math.PI - ((Math.PI - (beta * Math.PI)/360)/2) - (Math.PI/4);
-var b = 2*l*Math.sin(beta/2);
-var x = b*Math.sin(gam);
-var y = b*Math.cos(gam);
-var p1 = Math.cos(beta);
-var p2 = Math.sin(beta);
 
 var canvas = document.getElementById(planeta_id);
 var ctx = canvas.getContext("2d");
@@ -36,18 +25,16 @@ var imageHeight = 0;
 var drawPlaneta = function(){
 
 ctx.clearRect(0, 0, width, height);
-// применяем к нашей планете вращение
-ctx.transform(p1, p2, -p2, p1, x, -y);
 // рисуем карту с новыми координатами внутри элемента
 ctx.drawImage(image, imageWidth, imageHeight, width, height, 0, 0, width, height); 
 //добавляем тень и блики
-ctx.drawImage(fxShadow, 0, 0, width, height);
+ctx.drawImage(img, 0, 0, width, height);
 // если смещение достигло предела, начинаем сначала
-if (imageWidth >= 300) imageWidth = 0; 
+if (imageWidth >= 360) imageWidth = 0; 
 else imageWidth = imageWidth+0.5; // иначе двигаем карту дальше
 
 }
 
-setInterval(drawPlaneta, 50); // запускаем анимацию со скоростью 50 мс.
+setInterval(drawPlaneta, 40); // запускаем анимацию со скоростью 40 мс.
 
 });
